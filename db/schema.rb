95efec0c4d890eb8cd4b589b8efef32cbbda7bdc
Emma Cheng <emmayauyau@gmail.com>
2022-02-22 15:45:05 +0000

    add traits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_153619) do
+ActiveRecord::Schema.define(version: 2022_02_22_154415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(version: 2022_02_22_153619) do
     t.integer "price"
     t.string "description"
     t.string "text"
+    t.integer "strength"
+    t.integer "humour"
+    t.integer "romance"
+    t.integer "kindness"
     t.index ["user_id"], name: "index_supers_on_user_id"
   end
 
