2a9933b6e0d2a9748bc021d3db0140cf2c880ae7
Emma Cheng <emmayauyau@gmail.com>
2022-02-22 16:32:59 +0000

    add new to SuperDate and update routes

--- app/controllers/super_dates_controller.rb
+++ app/controllers/super_dates_controller.rb
@@ -1,8 +1,12 @@
 class SuperDatesController < ApplicationController
+  before_action :find_super
+
+  def new
+    @super_date = SuperDate.new
+  end
 
   def create
     @super_date = SuperDate.new(set_params)
-    @super = Super.find(params[:super_id])
     @super_date.super = @super
     if @super_date.save
       redirect_to super_path
@@ -12,6 +16,10 @@ class SuperDatesController < ApplicationController
   end
 
   private
+
+  def find_super
+    @super = Super.find(params[:super_id])
+  end
 
   def set_params
     params.require(:super_date).permit(:super_id, :user_id, :date)
