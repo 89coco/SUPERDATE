0a0bc60d53f90c94c9b613cf88fdca14bf255320
Emma Cheng <emmayauyau@gmail.com>
2022-02-22 15:37:37 +0000

    add description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_111218) do
+ActiveRecord::Schema.define(version: 2022_02_22_153619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2022_02_22_111218) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price"
+    t.string "description"
+    t.string "text"
     t.index ["user_id"], name: "index_supers_on_user_id"
   end
 
