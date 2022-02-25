98cf0025fc87139411227aa1de55864b128d6b5b
Emma Cheng <emmayauyau@gmail.com>
2022-02-25 13:57:45 +0000

    updated

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -4,6 +4,15 @@ const initStarRating = () => {
   $('#super_strength').barrating({
     theme: 'bars-square'
   });
+  $('#super_humour').barrating({
+    theme: 'bars-square'
+  });
+  $('#super_romance').barrating({
+    theme: 'bars-square'
+  });
+  $('#super_kindness').barrating({
+    theme: 'bars-square'
+  });
 };
 
 export { initStarRating };
