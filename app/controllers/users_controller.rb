22dccadcb5e85098e351c04b0e435fa87a381764
Carolin Schmidt <mail@cdschmidt.com>
2022-02-25 10:13:00 +0100

    added classes from index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @supers = current_user.supers
+    @super_dates = current_user.super_dates
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :super_dates
+  has_many :supers, through: :super_dates
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
